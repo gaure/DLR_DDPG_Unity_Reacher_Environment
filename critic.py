04204dee697f35ca16b5212940a0fe7ecb8501b9
Guillermo Aure <gaure72@gmail.com>
2018-11-27 22:58:09 -0500

    refactor
    Change the critic model and add the actions in the second hidden layer instead of the first

--- critic.py
+++ critic.py
@@ -9,9 +9,8 @@ class Critic(nn.Module):
                  state_size,
                  action_size,
                  seed,
-                 fc1_units=64,
-                 fc2_units=128,
-                 fc3_units=64):
+                 fc1_units=400,
+                 fc2_units=300):
         """
 
         :state_size: (int) Dimension of each state
@@ -25,41 +24,30 @@ class Critic(nn.Module):
         self.state_size = state_size
         self.action_size = action_size
 
-        # critic state hidden layers
+        # critic input state layers
         self.state_fc1 = nn.Linear(self.state_size, fc1_units)
         self.sln1 = nn.LayerNorm(fc1_units)
-        self.state_fc2 = nn.Linear(fc1_units, fc2_units)
-        self.sln2 = nn.LayerNorm(fc2_units)
 
-        # critic action hidden layers
-        self.action_fc1 = nn.Linear(self.action_size, fc1_units)
-        self.aln1 = nn.LayerNorm(fc1_units)
-        self.action_fc2 = nn.Linear(fc1_units, fc2_units)
-        self.aln2 = nn.LayerNorm(fc2_units)
-
-        # critic aggregate layers
-        self.aggregate_fc1 = nn.Linear(fc2_units, fc3_units)
-        self.aggregate_ln1 = nn.LayerNorm(fc3_units)
+        # critic temp networks to join actions and states
+        self.temp1 = nn.Linear(fc1_units, fc2_units)
+        self.temp2 = nn.Linear(action_size, fc2_units)
+        self.temp_norm = nn.LayerNorm(fc2_units)
 
         # critic output
-        self.fc_output = nn.Linear(fc3_units, 1)
+        self.fc_output = nn.Linear(fc2_units, 1)
 
     def forward(self, state, action):
         # state path
         s = F.relu(self.sln1(self.state_fc1(state)))
-        s = F.relu(self.sln2(self.state_fc2(s)))
 
-        # action path
-        a = F.relu(self.aln1(self.action_fc1(action)))
-        a = F.relu(self.aln2(self.action_fc2(a)))
+        # merging path
+        t1 = self.temp1(s)
+        t2 = self.temp2(action)
 
-        # merge by adding both path and producting tensor same size
-        # equivalent to keras.Add()
-        x = s + a
+        # add the actions to the states by concatenation
+        merged = F.relu(self.temp_norm(t1 + t2))
 
-        # Pass the above result through a relu function and produce
-        # a one output node with no activation.
-        x = F.relu(self.aggregate_ln1(self.aggregate_fc1(x)))
-        q_values = self.fc_output(x)
+        # get the q_value
+        q_value = self.fc_output(merged)
 
-        return q_values
+        return q_value
